59be44edea72753a0821b96bfe4f8ff12763f0ea
Joko <kalle0045@gmail.com>
2024-07-06 08:43:51 +0200

    add date

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import './App.css'
-import { Input } from './components/ui/input'
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@radix-ui/react-popover";
+import { format, setDate as setDateFn } from "date-fns"; // Renamed to avoid conflict
+import "./App.css";
+import { Calendar } from "./components/ui/calendar";
+import { useState } from "react";
+import { Input } from "./components/ui/input";
 
 function App() {
+  const [date, setDate] = useState<Date>();
 
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Input type="text" placeholder="Type something..." />
-    </>
-  )
+    <div>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Input
+            value={date && format(date, "PPP")}
+            readOnly
+            type="text"
+            placeholder="Enter a date"
+          />
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
 }
 
-export default App
+export default App;
