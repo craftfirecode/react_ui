2d7bca8e7c393d3047ae1d2ad880cd1c392ce9ad
Joko <kalle0045@gmail.com>
2024-07-06 08:06:25 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Input } from './components/ui/input'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Input type="text" placeholder="Type something..." />
+    </>
+  )
+}
+
+export default App
