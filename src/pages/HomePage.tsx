3576f7967c487e5cbd64b5d821319a5a2685344e
Joko <kalle0045@gmail.com>
2024-07-07 09:50:54 +0200

    refactor: Update stylesheets imports and add generatedStyles.css

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 const HomePage = () => {
   return (
-    <div className="container m-[100px]">
+    <div className="container p-[150px] bg-[#007bff] text-[150px] m-[50px]">
         <div className="row">
           <div className="col bg-info">1</div>
           <div className="col-auto bg-danger">2222</div>
